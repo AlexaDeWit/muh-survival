f5567ac566b71ac3ea1d666b2b693b3cf964c7d3
Alexa DeWit <alexa.dewit@gmail.com>
2015-06-12 14:40:07 +0200

    Initial Commit

---
+++ js/game_code.js
@@ -0,0 +1,12 @@
+$(document).ready( function(){
+  //Game code goes here
+
+  var inputBox = $("#inpputBox").first();
+  var textOutput = $("#textOutput").first();
+  
+  textOutput.append( formatOutput("cool!") );
+});
+
+function formatOutput(outputString) {
+  return "<li>" + outputString + "</li>";
+};
