9b38be45e14aa2766b9b0557b427555e8840417c
Alexa DeWit <alexa.dewit@gmail.com>
2015-06-13 18:45:39 +0200

    Multiple nouns/verbs implemented and working.

--- js/models/room.js
+++ js/models/room.js
@@ -2,21 +2,24 @@ var Room = Backbone.Model.extend({
 
   findAction : function( actionWords ){
     return _.find( this.get( "possible_actions" ), function( possAct ){
+      console.log( possAct);
       var isVerbMatched,
           isNounMatched;
       if( possAct.hasOwnProperty( "verbs" ) ){
         //Look for allowed verbs in user input
-        isVerbMatched = _.fold( possAct.verbs, function( verb, isFound ){
-          return isFound && _.contains( actionWords, verb );
+        isVerbMatched = _.foldl( possAct.verbs, function( verb, isFound ){
+          return isFound || _.contains( actionWords, verb );
         }, false);
 
+        console.log( isVerbMatched );
         //Noun and Verb required.
         if( possAct.hasOwnProperty( "nouns" ) ){
           //Look for allowed nouns in user input
-          isNounMatched = _.fold( possAct.nouns, function( nouns, isFound ){
-            return isFound && _.contains( actionWords, noun );
+          isNounMatched = _.foldl( possAct.nouns, function( noun, isFound ){
+            return isFound || _.contains( actionWords, noun );
           }, false);
 
+          console.log( isNounMatched );
           return isVerbMatched && isNounMatched;
         //Verb only
         } else {
