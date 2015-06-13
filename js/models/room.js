5e1a3b7ca33339707fa00f4346ff37a33907248b
Alexa DeWit <alexa.dewit@gmail.com>
2015-06-13 18:50:54 +0200

    Bugfix

--- js/models/room.js
+++ js/models/room.js
@@ -2,28 +2,26 @@ var Room = Backbone.Model.extend({
 
   findAction : function( actionWords ){
     return _.find( this.get( "possible_actions" ), function( possAct ){
-      console.log( possAct);
+
       var isVerbMatched,
           isNounMatched;
       if( possAct.hasOwnProperty( "verbs" ) ){
         //Look for allowed verbs in user input
-        isVerbMatched = _.foldl( possAct.verbs, function( verb, isFound ){
+        isVerbMatched = _.foldl( possAct.verbs, function( isFound, verb ){
           return isFound || _.contains( actionWords, verb );
         }, false);
 
-        console.log( isVerbMatched );
         //Noun and Verb required.
         if( possAct.hasOwnProperty( "nouns" ) ){
           //Look for allowed nouns in user input
-          isNounMatched = _.foldl( possAct.nouns, function( noun, isFound ){
+          isNounMatched = _.foldl( possAct.nouns, function( isFound, noun ){
             return isFound || _.contains( actionWords, noun );
           }, false);
 
-          console.log( isNounMatched );
           return isVerbMatched && isNounMatched;
         //Verb only
         } else {
-          return true;
+          return isVerbMatched;
         }
       } else {
         return undefined;
