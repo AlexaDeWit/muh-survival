ea227b256ef1907ba35340a8218d0fb4e5fa1bf9
Alexa DeWit <alexa.dewit@gmail.com>
2015-06-13 18:40:41 +0200

    Adds engine code for plural verbs/nouns

--- js/models/room.js
+++ js/models/room.js
@@ -2,12 +2,26 @@ var Room = Backbone.Model.extend({
 
   findAction : function( actionWords ){
     return _.find( this.get( "possible_actions" ), function( possAct ){
-      //Verb only
-      if( possAct.hasOwnProperty( "verb" ) && !possAct.hasOwnProperty("noun") ){
-        return _.contains( actionWords, possAct.verb );
-      } else if( possAct.hasOwnProperty( "verb" ) && possAct.hasOwnProperty("noun") ){
-        return _.contains( actionWords, possAct.verb ) &&
-               _.contains( actionWords, possAct.noun );
+      var isVerbMatched,
+          isNounMatched;
+      if( possAct.hasOwnProperty( "verbs" ) ){
+        //Look for allowed verbs in user input
+        isVerbMatched = _.fold( possAct.verbs, function( verb, isFound ){
+          return isFound && _.contains( actionWords, verb );
+        }, false);
+
+        //Noun and Verb required.
+        if( possAct.hasOwnProperty( "nouns" ) ){
+          //Look for allowed nouns in user input
+          isNounMatched = _.fold( possAct.nouns, function( nouns, isFound ){
+            return isFound && _.contains( actionWords, noun );
+          }, false);
+
+          return isVerbMatched && isNounMatched;
+        //Verb only
+        } else {
+          return true;
+        }
       } else {
         return undefined;
       }
